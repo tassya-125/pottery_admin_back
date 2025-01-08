19ad15a2b16dc1e0477869a58a13c01b605b6385
72199187 <72199187@bbktel.com>
2025-01-08 11:15:07 +0800

    项目创建

---
+++ src/main/java/org/example/pottery_admin_back/PotteryAdminBackApplication.java
@@ -0,0 +1,13 @@
+package org.example.pottery_admin_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotteryAdminBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PotteryAdminBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/pottery_admin_back/PotteryAdminBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.pottery_admin_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PotteryAdminBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
